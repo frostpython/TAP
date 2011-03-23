60e3b2cd93e4256ebad8a9ac387facb17ed14ff0
diablo <diablo@christian-bale.(none)>
2011-03-23 18:54:45 -0500

    Implemented DiceBag functionality and cleaned the menus a bit

--- tap.c
+++ tap.c
@@ -1,3 +1,13 @@
+/*
+This file is part of TAP.
+
+TAP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+TAP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with TAP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // Included because... duh.
 #include <stdio.h>
 // Included for rand()
@@ -5,31 +15,82 @@
 // Included for the random seed
 #include <time.h>
 
-int sumDice(int sides, int number)
+int SumDice(int sides, int number, int verbose)
+// Accepts number of sides on each die, number of dice to roll, and whether you want to hear each roll
 {
-   int i;
+   int i, num;
    int sum=0;
+
+   // Puts a little whitespace on the screen. This will need to be cleaned later
+   if ( verbose != 0 ) {
+      printf("\n");
+   }
    // Loops through the number of dice we want to roll and sums them
    for (i = 1; i <= number; i++) {
       // rand() gives a big number, so I modulate by 10 and then add one so it starts at 1
-      sum += ( rand() % sides + 1 );
+      num = ( rand() % sides + 1 );
+      if ( verbose != 0 ) {
+         printf("This roll is %d\n",num);
+      }
+      sum += num;
    }
    return sum;
 }
-int main()
+
+int DiceBag()
 {
    // Seeds the random number
    srand(time(NULL));
-
    int numDice,sides;
+
+   // The clears the screen. It seems a little hacky to work on Windoze too, but it works. Fix maybe?
+   if (system("cls")) system("clear");
 
    printf("How many dice would you like to roll?\n");
    scanf("%d",&numDice);getchar();
-   printf("%d\n",numDice);
    printf("How many sides on them?\n");
    scanf("%d",&sides);getchar();
-   printf("%d\n",sides);
-   printf("The result is %d\n",sumDice(sides,numDice));
+   printf("\n\nThe result is %d\n",SumDice(sides,numDice,1));
+
+   return 0;
+}
+
+int main()
+{
+
+   int menuNum;
+
+   while ( menuNum < 0 || menuNum > 5 ) {
+      fflush(stdin);
+      printf("Welcome to TAP!\n");
+      printf("1. Dice Bag\n");
+      printf("2. Character Creation\n");
+      printf("3. Item Creation\n");
+      printf("4. Campaign Creation\n");
+      printf("5. Settings\n");
+      printf("0. Quit\n");
+
+      scanf("%d",&menuNum);getchar();
+   }
+   switch ( menuNum) {
+   case 1:
+      DiceBag();
+      break;
+   case 2:
+   case 3:
+   case 4:
+   case 5:
+      printf("Oops! This isn't implemented yet. Poke us in IRC to let us know you want it\n");
+      break;
+   case 0:
+      printf("Thanks for playing!\n");
+      exit;
+      break;
+   default:
+      printf("Seems like you have mistyped. Please try again. 0 will let you exit\n");
+      break;
+   }
+
 
    return 0;
 }
